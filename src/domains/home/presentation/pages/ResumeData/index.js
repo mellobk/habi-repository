af6d9a0b2a06806a49105047a514f8842e87e0d8
mellobk <65514817+mellobk@users.noreply.github.com>
2022-07-03 07:14:06 -0500

    changes on resumen reset redux and delete localstorage info

--- src/domains/home/presentation/pages/ResumeData/index.js
+++ src/domains/home/presentation/pages/ResumeData/index.js
@@ -1,17 +1,24 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import ButtonApp from "../../../../../shared/presentation/components/Button";
+import { userInfoInit } from "../../../application/constans/formsHomeFields";
+import { setUserInfo } from "../../../application/slices/home";
 import { ThankYouRoute } from "../../../infrastructure/routes";
 import ResumeView from "../../components/ResumeView";
 
 import "./ResumeData.scss";
 
 const ResumeData = () => {
+  const dispatch = useDispatch();
   const resumeHistory =  useHistory();
   const handleClick = () =>{
+    dispatch(setUserInfo({ ...userInfoInit }))
+    window.localStorage.removeItem('userInfo');
     resumeHistory.push(ThankYouRoute)
 }
 const handleClickBack = () =>{
+  
   resumeHistory.goBack()
 }
 
